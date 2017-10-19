d53bd06dcad5c36451bb2a7a0126806339ba56e5
c153197984 <c153197984@users.noreply.github.com>
2017-10-19 22:46:21 +0800

    Add files via upload

---
+++ selpg.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+	"bufio"
+	"os/exec"
+	"strings"
+)
+
+type selpgArgs struct {
+	start int
+	end   int
+	pagelen   int
+	page_seperator bool
+	dest string
+	inputFile string
+}
+
+var progname string
+
+
+func getSelpg(arg *selpgArgs) {
+	flag.IntVar(&(arg.start), "s", -1, "Start page..")
+	flag.IntVar(&(arg.end), "e", -1, "End page.")
+	flag.IntVar(&(arg.pagelen), "l", 72, "Line number per page.")
+	flag.BoolVar(&(arg.page_seperator), "f", false, "use [-f=true] to seperate pages")
+	flag.StringVar(&(arg.dest), "d", "", "Destionation of output.")
+
+	flag.Parse()
+
+	if arg.start == -1 || arg.end == -1 {
+		printErr("not enough arguments!")
+	}
+	if len(flag.Args()) > 1 {
+		printErr("can't read more than one file!")
+	}
+	if arg.start < 1 {
+		printErr("start page can't less than 1!")
+	}
+	if arg.end < arg.start {
+		printErr("end page need grater than or equal to start page!")
+	}
+	if arg.pagelen < 1 {
+		printErr("page length can't less than 1!")
+	}
+
+	if len(flag.Args()) == 0 {
+		arg.inputFile = ""
+	} else {
+		arg.inputFile = flag.Args()[0]
+	}
+}
+
+func printErr(err string) {
+	fmt.Fprintf(os.Stderr, err+"\n"+
+		"\nUSAGE: %s -s start_page -e end_page [-f=true|false | -l pageline] [-d dest] [inputFileName]\n", progname)
+	os.Exit(1)
+}
+
+func run() {
+	var args selpgArgs
+	progname := os.Args[0]
+	getSelpg(&args)
+
+	var err error
+	var cmd *exec.Cmd
+	fin := os.Stdin
+	fout := os.Stdout
+	if args.inputFile != "" {
+		fin, err = os.Open(args.inputFile)
+		if err != nil {
+			printErr("can't open file\"" + args.inputFile + "\"!")
+		}
+	}
+	if args.dest != "" {
+		t := fmt.Sprintf("%s", args.dest)
+		cmd = exec.Command("sh", "-c", t)
+		if err != nil {
+			printErr("can't open file to \"" + t + "\"!")
+		}
+	}
+
+	var line string
+	pageNum := 1
+	inReader := bufio.NewReader(fin)
+	outfile_text := ""
+	if args.page_seperator == false {
+		lineNum := 0
+
+		for true {
+			line, err = inReader.ReadString('\n')
+			if err != nil {
+				break
+			}
+			lineNum++
+			if lineNum > args.pagelen {
+				pageNum++
+				lineNum = 1
+			}
+			if pageNum >= args.start && pageNum <= args.end {
+				if args.dest == "" {
+					fmt.Fprintf(fout, "%s", line)
+				} else {
+					outfile_text += line
+				}
+			}
+		}
+	} else {
+		for true {
+			c, _, erro := inReader.ReadRune()
+			if erro != nil {
+				break
+			}
+			if c == '\f' {
+				pageNum++
+			}
+			if pageNum >= args.start && pageNum <= args.end {
+				if args.dest == "" {
+					fmt.Fprintf(fout, "%c", c)
+				} else {
+					outfile_text += string(c)
+				}
+			}
+		}
+	}
+
+	if args.dest != "" {
+		cmd.Stdin = strings.NewReader(outfile_text)
+		cmd.Stdout = os.Stdout
+		err = cmd.Run()
+		if err != nil {
+			printErr("print error!")
+		}
+	}
+
+	if pageNum < args.start {
+		fmt.Fprintf(os.Stderr, "%s: start_page (%d) greater than total pages (%d), no output\n", progname, args.start, pageNum)
+	} else {
+		if pageNum < args.end {
+			fmt.Fprintf(os.Stderr, "%s: end_page (%d) greater than total pages (%d), less output\n", progname, args.end, pageNum)
+		}
+	}
+
+	fin.Close()
+	fout.Close()
+	fmt.Fprintf(os.Stderr, "%s: done\n", progname)
+}
+
+
+func main() {
+	run()
+}
